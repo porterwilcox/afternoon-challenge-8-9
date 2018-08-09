019dec4c5f342ee67e52e51c17f10ef69420a84c
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-09 16:21:15 -0600

    houses done starting on jobs

--- app/components/houses/HouseController.js
+++ app/components/houses/HouseController.js
@@ -1,20 +1,28 @@
 import HouseService from "./HouseService.js";
 
-let houseService = new HouseService();
+let houseService = new HouseService();;
 
-function drawHouses(){
+function listHouse() {
+    document.querySelector(".list-house").style.display = "block";
+}
+document.querySelector(".house-btn").addEventListener('click', listHouse);
+function closeHouse() {
+    document.querySelector(".list-house").style.display = "none";
+}
+document.querySelector(".close-house").addEventListener('click', closeHouse);
+
+function drawHouses() {
     let houses = houseService.getHouses();
     let template = '';
     for (let i = 0; i < houses.length; i++) {
         const house = houses[i];
-        template += `
-        <div class="row">
+        template += `        
         <div class="col-sm-6">
             <div class="card">
-                <img class="card-img-top" src="${house.image}" alt="your house">
+                <img class="card-img-top house-img" src="${house.image}" alt="your house">
                 <div class="card-body">
                     <h4 class="card-title">$${house.price}</h4>
-                    <h6 class="card-subtitle">${house.address}</h6>
+                    <h5 class="card-subtitle">${house.address}</h5>
                     <h6 class="card-subtitle">${house.city}, ${house.state} ${house.zip}</h6>
                     <ul class="list-group">
                         <li class="list-group-item">Floors: ${house.floors}</li>
@@ -25,22 +33,22 @@ function drawHouses(){
                     <p>Built in ${house.yearBuilt}</p>
                 </div>
             </div>
-        </div>
-    </div>
+        </div>    
         `
     }
     document.querySelector('.houses').innerHTML = template;
 }
 
 export default class HouseController {
-    constructor(){
+    constructor() {
         drawHouses();
     }
-    addHouse(e){
+    addHouse(e) {
         e.preventDefault();
-        let houseData = houseService.addHouse(e.target);
+        let houseData = e.target;
+        houseService.addHouse(houseData);
+        houseData.reset();
         drawHouses();
-        e.reset();
-        drawHouses();
+        closeHouse();
     }
 }
